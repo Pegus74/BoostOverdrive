2150ffef9133c08bd1dce3efd0983dfda0a9db9c
Egor <pegus74@mail.ru>
2025-10-02 17:02:33 +0500

    Добавлена первоночальная реализация системы стилей, добавлен индикатор перезарядки рывка

---
+++ Assets/Scripts/StyleController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerStyle
+{
+    public string styleName = "Style"; 
+    public float walkSpeed = 5f;  
+    public float sprintSpeed = 7f;  
+    public float jumpPower = 5f; 
+    public float fov = 60f;  
+}
+
+public class StyleController : MonoBehaviour
+{
+    public PlayerStyle[] styles = new PlayerStyle[2]; 
+    public KeyCode switchStyleKey = KeyCode.Tab; 
+    private int currentStyleIndex = 0;  
+
+    private FirstPersonController controller;
+    private Camera playerCamera; 
+    void Awake()
+    {
+        controller = GetComponent<FirstPersonController>();
+        playerCamera = controller.playerCamera;  
+
+        if (styles.Length == 0)
+        {
+            styles = new PlayerStyle[2];
+            styles[0] = new PlayerStyle { styleName = "Legs style", walkSpeed = 10f, sprintSpeed = 15f, jumpPower = 10f, fov = 60f };
+            styles[1] = new PlayerStyle { styleName = "Hands style", walkSpeed = 6f, sprintSpeed = 8f, jumpPower = 6f, fov = 90f };
+        }
+
+        ApplyStyle(currentStyleIndex);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(switchStyleKey))
+        {
+            currentStyleIndex = (currentStyleIndex + 1) % styles.Length;  
+            ApplyStyle(currentStyleIndex);
+        }
+    }
+
+    private void ApplyStyle(int index)
+    {
+        if (index < 0 || index >= styles.Length) return;
+
+        PlayerStyle style = styles[index];
+
+        controller.walkSpeed = style.walkSpeed;
+        controller.sprintSpeed = style.sprintSpeed;
+        controller.jumpPower = style.jumpPower;
+        controller.fov = style.fov;
+        playerCamera.fieldOfView = style.fov;
+
+    }
+}
